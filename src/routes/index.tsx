a5695c81456ea2e5848fe7080c58a7d10af16ca4
Makje <markjay.lunas@gmail.com>
2026-04-14 13:59:34 +0800

    refactor: replace custom theme logic with shadcn-based ThemeProvider and update dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ThemeToggle from "@/components/ThemeToggle";
 import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
 	return (
-		<main className="page-wrap px-4 pb-8 pt-14">
+		<main className="px-4 pb-8 pt-14">
 			<Button>Button</Button>
+			<ThemeToggle />
 		</main>
 	);
 }
