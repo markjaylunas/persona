cea1a3f23ef082a711cd6fc611b5922ef5da4e5d
Makje <markjay.lunas@gmail.com>
2026-04-21 11:23:05 +0800

    style: improve mobile layout of header

--- src/components/layout/publish-header.tsx
+++ src/components/layout/publish-header.tsx
@@ -5,7 +5,7 @@ export default function PublishHeader() {
 	return (
 		<header className="flex justify-between items-center p-4 max-w-7xl mx-auto">
 			<div />
-			<div className="flex gap-2">
+			<div className="flex gap-2 justify-between sm:justify-end w-full">
 				<ThemeToggle />
 				<PublishButtons />
 			</div>
