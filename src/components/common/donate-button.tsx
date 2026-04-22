ce5f26321bf6ac22632cf027db02599807aa1a68
Makje <markjay.lunas@gmail.com>
2026-04-22 21:47:53 +0800

    refactor: replace Button component with anchor tag using buttonVariants for external link navigation

--- src/components/common/donate-button.tsx
+++ src/components/common/donate-button.tsx
@@ -1,11 +1,16 @@
 import { Heart } from "lucide-react";
-import { Button } from "../ui/button";
+import { buttonVariants } from "../ui/button";
 
 export default function DonateButton() {
 	return (
-		<Button size="lg">
+		<a
+			href="https://ko-fi.com/makje"
+			target="_blank"
+			rel="noopener noreferrer"
+			className={buttonVariants({ size: "lg" })}
+		>
 			<Heart className="size-4 mr-0.5" />
 			<span className="sr-only sm:not-sr-only">Donate</span>
-		</Button>
+		</a>
 	);
 }
